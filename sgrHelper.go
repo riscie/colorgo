ea963e9490c90a47c8d7a0dd14c485ff827d6657
Matthias Langhard <matthias@langaust.com>
2016-01-15 13:28:28 +0100

    more colors, go fmt golint go vet

--- sgrHelper.go
+++ sgrHelper.go
@@ -1,17 +1,34 @@
 package main
 
 const (
-	RED  = "\x1b[38;05;1m"
-	BLUE = "\x1b[38;05;4m"
+	fgBlack   = "\x1b[38;05;0m"
+	fgRed     = "\x1b[38;05;1m"
+	fgGreen   = "\x1b[38;05;2m"
+	fgYellow  = "\x1b[38;05;3m"
+	fgBlue    = "\x1b[38;05;4m"
+	fgMagenta = "\x1b[38;05;5m"
+	fgCyan    = "\x1b[38;05;6m"
+	fgGrey    = "\x1b[38;05;7m"
+	fgWhite   = "\x1b[38;05;255m"
 
-	BOLD  = "\x1b[1m"
-	RESET = "\x1b[0m"
+	bgBlack   = "\x1b[48;05;0m"
+	bgRed     = "\x1b[48;05;1m"
+	bgGreen   = "\x1b[48;05;2m"
+	bgYellow  = "\x1b[48;05;3m"
+	bgBlue    = "\x1b[48;05;4m"
+	bgMagenta = "\x1b[48;05;5m"
+	bgCyan    = "\x1b[48;05;6m"
+	bgGrey    = "\x1b[48;05;7m"
+	bgWhite   = "\x1b[48;05;255m"
+
+	bold  = "\x1b[1m"
+	reset = "\x1b[0m"
 )
 
 func sgrBoldRed(text string) string {
-	return RED + BOLD + text + RESET
+	return fgMagenta + bold + text + reset
 }
 
 func sgrBoldBlue(text string) string {
-	return BLUE + BOLD + text + RESET
+	return fgYellow + bold + text + reset
 }
